8335921c4590a7f275d9adaaf10f187e33a5b28f
Shaik Zaid <shaikzaid9393@gmail.com>
2025-08-20 21:05:10 +0530

    DataValidation Completed

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import sys
 from networksecurity.logging.logger import logging
-def initiate_data_ingestion(self):
-    try:
-        df = self.export_collection_as_dataframe()
-        df = self.export_data_to_feature_store(df)
-        train_file_path, test_file_path = self.split_data_as_train_test(df)
-        artifact = DataIngestionArtifact(
-            train_file_path=train_file_path,
-            test_file_path=test_file_path
-        )
-        return artifact
-    except Exception as e:
-        raise NetworkSecurityException(e, sys)   
-    
+from networksecurity.exception.exception import NetworkSecurityException
+
+# Import configs
+from networksecurity.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig
+# Import components
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
+# Import artifacts
+from networksecurity.entity.artifact_entity import DataIngestionArtifact
 
 if __name__=='__main__':
-   
-    trainingpipelineconfig=TrainingPipelineConfig()
-    dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
-    data_ingestion=DataIngestion(dataingestionconfig)
-    logging.info("Initiate the data ingestion")
-    dataingestionartifact=data_ingestion.initiate_data_ingestion()
-    logging.info("Data Initiation Completed")
-    print(dataingestionartifact)
-    
-    
-    
-    
-    
+    try:
+        trainingpipelineconfig = TrainingPipelineConfig()
+
+        # Data Ingestion
+        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion = DataIngestion(dataingestionconfig)
+        logging.info("Initiate the data ingestion")
+        dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Ingestion Completed")
+        print(dataingestionartifact)
+
+        # Data Validation
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
